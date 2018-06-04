5d75248f713290b03530caffaeb45b6d4595748a
vinson aiono <vinson.aiono@gmail.com>
2018-06-04 14:55:05 -0700

    inital commit

---
+++ FizzBuzz.java
@@ -0,0 +1,8 @@
+public class FizzBuzz {
+    public static void main(String[] args){
+        Test test = new Test();
+        String answer = (String)test.fizzBuzz(2);
+        System.out.println(answer);
+    }
+
+}
